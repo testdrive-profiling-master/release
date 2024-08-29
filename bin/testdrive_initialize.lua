9e9a678ab377aa83c544a5594dd96c27839bd777
HyungKi Jeong <clonextop@gmail.com>
2024-08-29 10:13:27 +0900

    tgui 추가

--- bin/testdrive_initialize.lua
+++ bin/testdrive_initialize.lua
@@ -92,6 +92,7 @@ RequireMingwPackage("tree")											-- 2024/7
 RequireMingwPackage("whois")										-- 2024/7
 RequireMingwPackage("unrar")										-- 2024/7
 RequireMingwPackage("mingw-w64-ucrt-x86_64-libevent")				-- 2024/8
+RequireMingwPackage("mingw-w64-ucrt-x86_64-tgui")					-- 2024/8
 
 -------------------------------------------------------
 -- check notepad++ installation
